c2b1b78392baa6a5314ac563c54499855d84544e
“Roberto <roberto912003@gmail.com>
2025-11-21 20:32:02 +0100

    Browse nepremicnine
    Ko se ustvari nov listing potem nam izpše new listing v Kupi

--- Pages/Properties/Browse.cshtml.cs
+++ Pages/Properties/Browse.cshtml.cs
@@ -1,20 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using RZ_nepremicnine.Data;
+using RZ_nepremicnine.Models;
 
 namespace RZ_nepremicnine.Pages.Properties
 {
     public class BrowseModel : PageModel
     {
         private readonly ILogger<BrowseModel> _logger;
+        private readonly AppDbContext _context;
 
-        public BrowseModel(ILogger<BrowseModel> logger)
+        public BrowseModel(ILogger<BrowseModel> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
+        }
+    
+            // TODO: Load available properties for browsing
+            public List<Nepremicnina> Nepreminine { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Nepreminine = await GetAllNepremicnineAsync();
         }
 
-        public void OnGet()
+        //Simple dobi vse nepremicnine
+        public async Task<List<Nepremicnina>> GetAllNepremicnineAsync()
         {
-            // TODO: Load available properties for browsing
+            return await _context.Nepremicnine.ToListAsync();
         }
+
+                    
     }
 }
