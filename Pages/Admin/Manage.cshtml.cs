47a86a4a6040f06c2fc8cf4cebac3c842ca49c82
ZanelMiletic <zanel.miletic@student.um.si>
2026-01-03 17:49:37 +0100

    dodajanje manage accounta

--- Pages/Admin/Manage.cshtml.cs
+++ Pages/Admin/Manage.cshtml.cs
@@ -1,13 +1,31 @@
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RZ_nepremicnine.Models;
+using System.Threading.Tasks;
 
 namespace RZ_nepremicnine.Pages.Admin
 {
     public class ManageModel : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<Uporabniki> _userManager;
+
+        public ManageModel(UserManager<Uporabniki> userManager)
         {
-            // TODO: Implement admin management functionality
+            _userManager = userManager;
+        }
+
+        public string FullName { get; set; }
+        public string Email { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user != null)
+            {
+                FullName = user.FullName;
+                Email = user.Email;
+            }
         }
     }
 }
